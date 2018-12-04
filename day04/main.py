2166af76e30117dfd3ebfdc58e2f573a65b2f5c5
Geoffrey Hunter <ghunter@urthecast.com>
2018-12-04 09:24:16 -0800

    Finished day 4 part 1.

--- day04/main.py
+++ day04/main.py
@@ -1,28 +1,52 @@
+
+def get_minute(line):
+    colon_pos = line.find(':') 
+    minute_str = line[colon_pos + 1: colon_pos + 3]
+    return int(minute_str)
+
 input = []
 with open('input.txt', 'r') as file:
     for line in file:
         input.append(line.strip())
 
 input.sort()
-print(input)
 
 guard_sleep_times = {}
+total_guard_sleep_time = {}
 curr_min = 0
 for line in input:
-    print(line)
-
-    # date = line[1:11]
-    # print(date)
-
     hash_index = line.find('#')
     if hash_index != -1:
-        print('New guard')
         guard_id = line[hash_index + 1: line.find(' ', hash_index)]
-        print(guard_id)
 
         if not guard_id in guard_sleep_times:
             guard_sleep_times[guard_id] = [0]*60 
+            total_guard_sleep_time[guard_id] = 0
 
         curr_min = 0
+    elif line.find('asleep') != -1:
+        # Extract minute
+        minute = get_minute(line)
+        curr_min = minute
+            
+    elif line.find('wakes') != -1:
+        minute = get_minute(line)
+        for i in range(curr_min, minute):
+            guard_sleep_times[guard_id][i] += 1
+            total_guard_sleep_time[guard_id] += 1
 
-print(guard_sleep_times)
+        curr_min = minute
+
+# Find guard that slept the longest
+def keywithmaxval(d):
+     """ a) create a list of the dict's keys and values; 
+         b) return the key with the max value"""  
+     v=list(d.values())
+     k=list(d.keys())
+     return k[v.index(max(v))]
+
+key = keywithmaxval(total_guard_sleep_time)
+max_value = max(guard_sleep_times[key])
+max_index = guard_sleep_times[key].index(max_value)
+
+print(f'Guard {key} slept the most at minute {max_index}. Answer = {int(key)*max_index}')
