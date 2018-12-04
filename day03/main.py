a35113f68a9efd032654b54e3bceacd6185d2b26
Geoffrey Hunter <ghunter@urthecast.com>
2018-12-03 17:49:09 -0800

    Added day 3 part 2.

--- day03/main.py
+++ day03/main.py
@@ -9,13 +9,13 @@ grid = []
 for i in range(1000):
     grid.append([0]*1000)
 
+overlapping_claims = {}
 for claim in claims:    
     left_edge = int(claim[claim.find('@') + 2:claim.find(',')])
     top_edge = int(claim[claim.find(',') + 1: claim.find(':')])
 
     width = int(claim[claim.find(':') + 1: claim.find('x')])
     height = int(claim[claim.find('x') + 1:])
-    print(f'left_edge = {left_edge}, top_edge = {top_edge}, width = {width}, height = {height}')
 
     for i in range(left_edge, left_edge + width):
         for j in range(top_edge, top_edge + height):
@@ -28,3 +28,23 @@ for i in range(1000):
             count += 1
 
 print(f'count = {count}')
+
+# PART 2
+
+overlapping_claims = {}
+for claim in claims:
+    left_edge = int(claim[claim.find('@') + 2:claim.find(',')])
+    top_edge = int(claim[claim.find(',') + 1: claim.find(':')])
+
+    width = int(claim[claim.find(':') + 1: claim.find('x')])
+    height = int(claim[claim.find('x') + 1:])
+
+    overlapping_claims[claim] = False
+    for i in range(left_edge, left_edge + width):
+        for j in range(top_edge, top_edge + height):
+            if grid[i][j] > 1:
+                overlapping_claims[claim] = True
+
+for claim in claims:
+    if not overlapping_claims[claim]:
+        print(f'Non-overlapping claim = {claim}')
